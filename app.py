86d86ba65d0bf0189c17c73a91b9f9ebac27a608
maucasillasc-oss <maucasillasc@gmail.com>
2026-03-30 23:54:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,10 @@ analysis_status = {}
 def index():
     return render_template('index.html')
 
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
 @app.route('/upload', methods=['POST'])
 def upload_video():
     if 'video' not in request.files:
